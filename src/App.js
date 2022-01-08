13f580311d43aadb7f2886573ca0e36c24425d78
nikit-p <85838488+nikit-p@users.noreply.github.com>
2022-01-08 20:07:13 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Statewise from './components/stateWiseData/statewise';
+
+const App = () => {
+  return (
+    <>
+      <Statewise />
+    </>
+  )
+}
+
+export default App
+
